b0e781ca19ca743dcbe1577b48f4377ae2e79eae
Thisisus7 <rick840905528@gmail.com>
2024-07-20 21:10:59 +0800

    [Updating] multi-step prompts & code

--- src/config.py
+++ src/config.py
@@ -14,27 +14,37 @@ logging.basicConfig(
 GAMES = [
     {
         "name": "maze",
-        "prompt_path": "prompts/prompt_maze_os.txt",
+        "prompt_path": r"prompts\prompt_maze_os.txt",
+        "prompt_ms_path": r"prompts\prt_maze.txt",
+        "prompt_ms_history_path": r"prompts\prt_maze.txt",
         "image_path_format": r'data\maze\LevelImages\level_{}.png'
     },
     {
         "name": "sokoban",
-        "prompt_path": "prompts/prompt_sokoban_os.txt",
+        "prompt_path": r"prompts\prompt_sokoban_os.txt",
+        "prompt_ms_path": r"prompts\prt_sokoban.txt",
+        "prompt_ms_history_path": r"prompts\prt_sokoban_history.txt",
         "image_path_format": r'data\sokoban\LevelImages\level_{}.png'
     },
     {
         "name": "n-queens",
-        "prompt_path": "prompts/prompt_n_queens_os.txt",
+        "prompt_path": r"prompts\prompt_n_queens_os.txt",
+        "prompt_ms_path": r"prompts\prt_n_queens.txt",
+        "prompt_ms_history_path": r"prompts\prt_n_queens_history.txt",
         "image_path_format": r'data\n-queens\LevelImages\level_{}.png'
     },
     {
         "name": "sudoku",
-        "prompt_path": "prompts/prompt_sudoku_os.txt",
+        "prompt_path": r"prompts\prompt_sudoku_os.txt",
+        "prompt_ms_path": r"prompts\prt_sudoku.txt",
+        "prompt_ms_history_path": r"prompts\prt_sudoku._history.txt",
         "image_path_format": r'data\sudoku\LevelImages\level_{}.png'
     },
     {
         "name": "hanoi",
-        "prompt_path": "prompts/prompt_hanoi_os.txt",
+        "prompt_path": r"prompts\prompt_hanoi_os.txt",
+        "prompt_ms_path": r"prompts\prt_hanoi.txt",
+        "prompt_ms_history_path": r"prompts\prt_hanoi_history.txt",
         "image_path_format": r'data\sudoku\LevelImages\level_{}.png'
     },
 ]
@@ -42,4 +52,5 @@ GAMES = [
 # Configuration for inference levels and output directory
 START_LEVEL = 1
 END_LEVEL = 1
-OUTPUT_DIR = r'outputs\one_step\models\raw'
+OUTPUT_OS_DIR = r'outputs\one_step\models\raw'
+OUTPUT_MS_DIR = r'outputs\multi_step\models\raw'
--- src/one_step/infer_os.py
+++ src/one_step/infer_os.py
@@ -1,57 +1,12 @@
 import os
+import sys
 import json
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, Blip2Processor, Blip2ForConditionalGeneration, BitsAndBytesConfig
-from PIL import Image
 from typing import List
-from abc import ABC, abstractmethod
-from config import GAMES, START_LEVEL, END_LEVEL, OUTPUT_DIR
-import logging
+from model import QwenVLChatInferencer, BLIP2Inferencer
 
-class ModelInferencer(ABC):
-    @abstractmethod
-    def load_model(self):
-        pass
-
-    @abstractmethod
-    def infer(self, prompt: str, image_path: str) -> str:
-        pass
-
-    def cleanup(self):
-        if hasattr(self, 'model'):
-            del self.model
-        if hasattr(self, 'tokenizer'):
-            del self.tokenizer
-        elif hasattr(self, 'processor'):
-            del self.processor
-        torch.cuda.empty_cache()
-
-class QwenVLChatInferencer(ModelInferencer):
-    def load_model(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL-Chat-Int4", trust_remote_code=True)
-        self.model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat-Int4", device_map="cuda", trust_remote_code=True).eval()
-
-    def infer(self, prompt: str, image_path: str) -> str:
-        query = self.tokenizer.from_list_format([
-            {'image': image_path},
-            {'text': prompt},
-        ])
-        response, _ = self.model.chat(self.tokenizer, query=query, history=None)
-        return response
-
-class BLIP2Inferencer(ModelInferencer):
-    def load_model(self):
-        self.processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
-        quantization_config = BitsAndBytesConfig(load_in_8bit=True)
-        self.model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", device_map="auto", quantization_config=quantization_config).eval()
-
-    def infer(self, prompt: str, image_path: str) -> str:
-        raw_image = Image.open(image_path).convert('RGB')
-        inputs = self.processor(raw_image, prompt, return_tensors="pt").to("cuda", torch.float16)
-        with torch.inference_mode():
-            generation = self.model.generate(**inputs, max_new_tokens=100)
-            result = self.processor.decode(generation[0], skip_special_tokens=True).strip()
-        return result
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from config import GAMES, START_LEVEL, END_LEVEL, OUTPUT_OS_DIR as OUTPUT_DIR 
 
 class Game:
     def __init__(self, name: str, prompt_path: str, image_path_format: str):
